941d5887c06c5674241a6bb495a3c7c54d6d2433
Jeshua3545926 <garzaromerojeshuaabiram2019ktv@gmail.com>
2025-07-04 18:09:27 -0600

    Detect os

--- AstroDesktop/Launcher/Config.js
+++ AstroDesktop/Launcher/Config.js
@@ -2,13 +2,17 @@ const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 
 function createWindow () {
+  // Detectar el OS
+  const isWindows = process.platform === 'win32'
+
   const win = new BrowserWindow({
     width: 1200,
     height: 600,
     autoHideMenuBar: true,
+    frame: !isWindows, // Si es Windows: frame false (custom), sino frame true (nativo)
     webPreferences: {
       preload: path.join(__dirname, 'index.js')
-    }
+    } 
   })
 
   win.loadFile('index.html')
